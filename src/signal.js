6a1c2440c2c8516dbcc8bd51a44bf05f32879b58
Shuhei Kuno <circular.nine@gmail.com>
2024-09-08 04:39:30 +0900

    fix

---
+++ src/signal.js
@@ -0,0 +1,64 @@
+function dctII(x, N) {
+    let X = new Float32Array(N);
+    for (let k = 0; k < N; ++k) {
+        let sum = 0;
+        for (let n = 0; n < N; ++n) {
+            sum += x[n] * Math.cos(Math.PI / N * (n + 1 / 2) * k);
+        }
+        X[k] = sum;
+    }
+    return X;
+}
+
+function dctIII(x, N) {
+    let X = new Float32Array(N);
+    for (let k = 0; k < N; ++k) {
+        let sum = 1 / 2 * x[0];
+        for (let n = 1; n < N; ++n) {
+            sum += x[n] * Math.cos(Math.PI / N * (k + 1 / 2) * n);
+        }
+        X[k] = sum;
+    }
+    return X;
+}
+
+function dct2D(N, s) {
+    //const S = new Float32Array(N * N);
+    for (let u = 0; u < N; u++) {
+        for (let v = 0; v < N; v++) {
+            let sum = 0;
+            for (let x = 0; x < N; x++) {
+                for (let y = 0; y < N; y++) {
+                    sum += s[x + N * y] *
+                        Math.cos((2 * x + 1) * u * Math.PI / (2 * N)) *
+                        Math.cos((2 * y + 1) * v * Math.PI / (2 * N));
+                }
+            }
+            S[u + N * v] = 1 / 4 *
+                (u === 0 ? 1 / Math.SQRT2 : 1) *
+                (v === 0 ? 1 / Math.SQRT2 : 1) *
+                sum;
+        }
+    }
+    return S;
+}
+
+function idct2D(N, S) {
+    //const s = new Float32Array(N * N);
+    for (let x = 0; x < N; x++) {
+        for (let y = 0; y < N; y++) {
+            let sum = 0;
+            for (let u = 0; u < N; u++) {
+                for (let v = 0; v < N; v++) {
+                    sum += (u === 0 ? 1 / Math.SQRT2 : 1) *
+                        (v === 0 ? 1 / Math.SQRT2 : 1) *
+                        S[u + N * v] *
+                        Math.cos((2 * x + 1) * u * Math.PI / (2 * N)) *
+                        Math.cos((2 * y + 1) * v * Math.PI / (2 * N));
+                }
+            }
+            s[x + N * y] = 1 / 4 * sum;
+        }
+    }
+    return s;
+}
